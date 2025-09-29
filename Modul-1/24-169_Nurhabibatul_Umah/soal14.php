be1ccd066515990e88df907e756eb72425b23640
Nurhabibatul Umah <140476199+NURHABIBATULUMAH@users.noreply.github.com>
2025-09-29 16:28:29 +0700

    Add files via upload

---
+++ Modul-1/24-169_Nurhabibatul_Umah/soal14.php
@@ -0,0 +1,11 @@
+<!-- Fungsi dengan Argumen -->
+<?php
+function familyName($fname){
+    echo "$fname Refnes. <br>";
+}
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+?>
